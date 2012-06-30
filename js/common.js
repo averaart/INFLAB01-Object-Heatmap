f6af705936b956c3ccfa8fa44b82d73e7cc02813
Maarten van den Hoek <maartenvandenhoek@gmail.com>
2012-06-30 18:34:25 +0200

    Sort table on pearson, give pearson a color between red and green, update table on correlationrange slide

--- js/common.js
+++ js/common.js
@@ -122,54 +122,81 @@ function countAvgDeviationViolation(correlations) {
     }
 }
 
+/**
+ * HSV to RGB color conversion
+ *
+ * H runs from 0 to 360 degrees
+ * S and V run from 0 to 100
+ *
+ * Ported from the excellent java algorithm by Eugene Vishnevsky at:
+ * http://www.cs.rit.edu/~ncs/color/t_convert.html
+ */
+function hsvToRgb(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
 
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    s = Math.max(0, Math.min(100, s));
+    v = Math.max(0, Math.min(100, v));
 
+    // We accept saturation and value arguments from 0 to 100 because that's
+    // how Photoshop represents those values. Internally, however, the
+    // saturation and value are calculated from a range of 0 to 1. We make
+    // That conversion here.
+    s /= 100;
+    v /= 100;
 
+    if(s == 0) {
+        // Achromatic (grey)
+        r = g = b = v;
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
 
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
 
-//function fabricateCorrelation(item) {
-//    var c = {};
-//    c['avg_deviation     = item["avg_deviation"];
-//    c['pearsons          = item["pearsons"];
-//
-//    c['set_a = {};
-//    c['set_a.amountTotal = item["set_a"]["amount-total"];
-//    c['set_a.amountValue = item["set_a"]["amount-value"];
-//    c['set_a.attribute   = item["set_a"]["attribute"];
-//    c['set_a.set         = item["set_a"]["set"];
-//    c['set_a.value       = item["set_a"]["value"];
-//
-//    c['set_b = {};
-//    c['set_b.amountTotal = item["set_b"]["amount-total"];
-//    c.set_b.amountValue = item["set_b"]["amount-value"];
-//    c.set_b.attribute   = item["set_b"]["attribute"];
-//    c.set_b.set         = item["set_b"]["set"];
-//    c.set_b.value       = item["set_b"]["value"];
-//
-//    c.sub = item["sub"];
-//
-//    return c;
-//}
+    switch(i) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
 
-//
-//Correlation = {
-//    avg_deviation: 0,
-//    pearsons: 0,
-//    set_a: {
-//        amountTotal: 0,
-//        amountValue: 0,
-//        attribute: 0,
-//        set: '',
-//        value: ''
-//    },
-//    set_b: {
-//        amountTotal: 0,
-//        amountValue: 0,
-//        attribute: 0,
-//        set: '',
-//        value: ''
-//    },
-//    sub : new Array()
-//
-//
-//};
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+
+        default: // case 5:
+            r = v;
+            g = p;
+            b = q;
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
