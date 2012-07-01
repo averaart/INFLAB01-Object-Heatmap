c2d5277b86172abf8e74df8e19d8164c8e2c9449
Maarten van den Hoek <maartenvandenhoek@gmail.com>
2012-07-01 03:42:49 +0200

    Moved uploading to a different page

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import os
 from flask import Flask, render_template
+from flask.globals import request
 from upload import DataSetUploader
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('base.html')
+    return render_template('home.html')
 
-@app.route('/upload-data-set', methods=['POST'])
-def upload():
-    dsu = DataSetUploader()
-    dsu.upload()
-    return render_template('base.html')
+@app.route('/voeg-dataset-toe', methods=['GET', 'POST'])
+def newDataSet():
+    if request.method == 'POST':
+        dsu = DataSetUploader()
+        dsu.upload()
+        # ToDo: add dynamic notification
+        return render_template('upload.html', success=True)
+    else:
+        return render_template('upload.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
