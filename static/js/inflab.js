97c8673fe2d7678df4555759ea68b096c357f412
Maarten van den Hoek <maartenvandenhoek@gmail.com>
2012-07-02 15:40:29 +0200

    Analysis puts results in data table

--- static/js/inflab.js
+++ static/js/inflab.js
@@ -107,6 +107,32 @@ initAnalysisPage = function(){
         }
     );
 
+    $("#start-analysis").click(function() {
+        $("#loading-analysis-results").css('display', 'block');
+        $.ajax({ url: '/analyseer', type: 'POST'}).done(
+            function( data ) {
+                $("#loading-analysis-results").css('display', 'none');
+                data = $.parseJSON(data);
+                var dataToAdd = [];
+                $.each(data, function(key, item){
+                    dataToAdd.push([
+                        1,
+                        item["set_a"]["set"]+"<br>" +
+                            item["set_a"]["attribute"]+"<br>" +
+                            item["set_a"]["value"] +"<br>",
+                        item["set_b"]["set"]+"<br>" +
+                            item["set_b"]["attribute"]+"<br>" +
+                            item["set_b"]["value"] +"<br>",
+                        parseFloat(item["pearsons"]).toFixed(3)
+                    ]);
+                });
+                $('#analysis-results').dataTable().fnAddData(dataToAdd);
+            });
+    });
+
+
+
+
     /**
      * Make a data table of the results of the analysis.
      * It can sort on the third column (pearson correlation).
@@ -117,7 +143,12 @@ initAnalysisPage = function(){
         'bFilter': false,
         'aoColumnDefs': [
             { 'bSortable': false, 'aTargets': [ 0, 1, 2 ] }
-        ]
+        ],
+        "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+            console.log((Number(aData[3])+1.0)*60);
+            var rgb = hsvToRgb((Number(aData[3])+1.0)*20+40, 100, 100);
+            $('td:eq(3)', nRow).css( 'background-color', 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')');
+        }
     });
 }
 
@@ -143,4 +174,84 @@ function initMap(id) {
         disableDoubleClickZoom: false
     };
     map = new google.maps.Map(document.getElementById("map-area"), options);
+}
+
+
+/**
+ * HSV to RGB color conversion
+ *
+ * H runs from 0 to 360 degrees
+ * S and V run from 0 to 100
+ *
+ * Ported from the excellent java algorithm by Eugene Vishnevsky at:
+ * http://www.cs.rit.edu/~ncs/color/t_convert.html
+ */
+function hsvToRgb(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
+
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    s = Math.max(0, Math.min(100, s));
+    v = Math.max(0, Math.min(100, v));
+
+    // We accept saturation and value arguments from 0 to 100 because that's
+    // how Photoshop represents those values. Internally, however, the
+    // saturation and value are calculated from a range of 0 to 1. We make
+    // That conversion here.
+    s /= 100;
+    v /= 100;
+
+    if(s == 0) {
+        // Achromatic (grey)
+        r = g = b = v;
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
+
+    switch(i) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
+
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+
+        default: // case 5:
+            r = v;
+            g = p;
+            b = q;
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
