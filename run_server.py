35c1db7c8b04f10f8869a82705be78592e748379
Andra Veraart <averaart@hotmail.com>
2012-07-04 16:41:47 +0200

    Added logging to server.

--- run_server.py
+++ run_server.py
@@ -4,6 +4,13 @@ from app import app
 d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
 server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8080), d)
 
+if not app.debug:
+    import logging
+    from logging import FileHandler
+    file_handler = FileHandler("errors.log")
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
+
 if __name__ == '__main__':
     try:
         server.start()
